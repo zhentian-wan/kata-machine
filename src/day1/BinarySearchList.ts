834373e1c0e8c8878c5d1c26a6a31c125f29c0c0
zhentian-wan <answer881215@gmail.com>
2023-07-08 15:26:36 +0300

    save

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,26 @@
+/**
+ * low, high, mid
+ * do {
+ *  mid = (low + high) / 2
+ *  if (v === needle) return true
+ *  elif (v < needle) low = mid + 1
+ *  else high = mid - 1
+ * } while (low < high)
+ * always: [low, high)
+ */
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0, high = haystack.length;
 
+    do {
+        const mid = Math.floor((low + high) / 2)
+        const v = haystack[mid]
+        if (v === needle) return true
+        else if(v < needle) {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    } while (low < high)
+
+    return false
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,37 @@
+/**
+ * Noramlly we would consider find middle point of the array
+ * But if assume the first ball break, then we need to walk through
+ * half of the array to find the breaking point.
+ *
+ * There fore the T: O(N/2) = O(N).
+ *
+ * We want better than that.
+ *
+ * So what we can do is we take step of sqrt(N) each time for the first ball
+ * If it breaks, then we can walk through the sqrt(N) array to find the breaking point.
+ *
+ * O(sqrt(N)) < O(N)
+ */
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length))
+
+    // using first ball to find the breaking point range
+    let i = 0
+    while (jumpAmount * i < breaks.length) {
+        if (breaks[jumpAmount * i]) {
+            break
+        }
+        i++
+    }
+
+    // using second ball to find actual breaking point
+    let j = jumpAmount * (i - 1)
+    for (let k =0; k < jumpAmount; k++) {
+        if (breaks[j + k]) {
+            return j + k
+        }
+    }
+
+    return -1
 }
