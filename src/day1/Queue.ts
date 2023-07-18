6508fa557b8f70384dcc89e193cc6ea3ded6fea5
zhentian-wan <answer881215@gmail.com>
2023-07-18 10:00:17 +0300

    stack and queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,52 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
+// use a linked list to implement a queue
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const node = { value: item };
+        this.length++;
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
 
-}
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+
+        // free up memory
+        head.next = undefined;
+
+        // clean up tail
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
