739bf0d8bc68da2c1606fa842b3076dd72a6f001
zhentian-wan <answer881215@gmail.com>
2023-07-29 15:21:23 +0300

    BTB

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,21 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
 
+    while (q.length) {
+        const curr = q.shift()!;
+
+        if (curr.value === needle) {
+            return true;
+        }
+
+        if (curr.left) {
+            q.push(curr.left);
+        }
+
+        if (curr.right) {
+            q.push(curr.right);
+        }
+    }
+
+    return false;
 }
